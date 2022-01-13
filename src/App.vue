af37fc7775df17c4996fbf74b86115081c73629a
AHCHRISTIANSEN95 <a.christiansen1995@gmail.com>
2022-01-13 11:51:15 +0100

    translate to english

--- src/App.vue
+++ src/App.vue
@@ -22,14 +22,13 @@
 <template> 
   <div @mousemove="startMusic()">
     <dialog v-show="popUpOpen" class="dialog" :open="popUpOpen"> 
-      <h1>Tak for dine data.</h1>
+      <h1>THANK YOU FOR YOUR DATA </h1>
       <p>
-        De vil nu blive uploadet til det russiske dark web og solgt til højstbydende i cococoins, hvis du ikke fylder 4 kasser Underberg i de kreatives køleskab. Og det gælder alle dine data – også søgehistorikken. ”Happyslapping Nr. Aaby” – really, Richard?
-      </p>
+Your data will now be uploaded to the Russian dark web and sold to the highest bidder in cococoins. If not, you must fill the fridge in the creative department with 4 boxes of Underberg. This applies to all your data – including your search history. “Happy slapping Nr. Aaby” – really, Richard?       </p>
       <button @click="togglePopUp">OK</button>
     </dialog>
     <img src="./assets/www.png" alt="www" width="400" />
-    <h1>Nedtælling til næste snestorm - hvis ikke der er ryddet op inden</h1>
+    <h1>COUNTDOWN TO THE NEXT WINTER STORM – IF THERE’S NOT CLEANED UP BEFORE</h1>
     <vue3-flip-countdown
       mainColor="#93dbf4"
       mainFlipBackgroundColor="#1f2f37"
